569ea4392c8b64415ddf7801e3d2f4a91a21ed07
Akash Ananda Kumar <akashanandakumar75@gmail.com>
2021-07-10 13:59:16 +0530

    Update boston.py

--- Code/boston.py
+++ Code/boston.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-bdata=pd.read_csv(r"C:\Users\Asus\Downloads\0000000000002417_training_boston_x_y_train (1).csv")
+bdata=pd.read_csv(r"FOLDER_NAME.csv")
 bdata
 
 x_train_boston_data=bdata.iloc[:,0:13]
@@ -12,7 +12,7 @@ y_train_boston_data
 from sklearn.ensemble import GradientBoostingRegressor
 model=GradientBoostingRegressor()
 model.fit(x_train_boston_data,y_train_boston_data)
-boston_data1=np.loadtxt(r"C:\Users\Asus\Downloads\0000000000002417_test_boston_x_test (2).csv",delimiter = ',')
+boston_data1=np.loadtxt(r"FOLDER_NAMEcsv",delimiter = ',')
 y_predict=model.predict(boston_data1)
 np.savetxt("boston_predict.csv", y_predict)
 
